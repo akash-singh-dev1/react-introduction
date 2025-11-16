8e6857ad86861289544200c646fef7258e9989f8
Akash Singh <akashsingh.dev1@gmail.com>
2025-11-16 13:01:48 +0530

    added two component in the project Header.jsx and CoreConcept.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,28 @@
+import React from "react";
+import Header from "./components/Header.jsx";
+import CoreConcept from "./components/CoreConcept.jsx";
+import { CORE_CONCEPTS } from "./data";
 function App() {
   return (
     <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
+      <Header />
       <main>
+        {/*section for coreconcept */}
+        <section id="core-concepts">
+          <h2> core concept</h2>
+          <ul>
+            {CORE_CONCEPTS.map((obj) => {
+              console.log("map function is executed");
+              return (
+                <CoreConcept
+                  img={obj.image}
+                  title={obj.title}
+                  description={obj.description}
+                />
+              );
+            })}
+          </ul>
+        </section>
         <h2>Time to get started!</h2>
       </main>
     </div>
